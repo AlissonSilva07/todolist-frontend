cb3c197ab2ef3161050a0a64f67d5f6e7eb1cf76
Kássio Sá <kassio.slva@gmail.com>
2024-05-30 13:14:25 -0300

    feat: add and config react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
+
 export function App() {
-  return <h1>Todo List App</h1>
+  return <RouterProvider router={router} />
 }
