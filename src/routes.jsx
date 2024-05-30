0c966c3b4300d279d0c1ec67dc515607c127b720
Kássio Sá <kassio.slva@gmail.com>
2024-05-30 13:37:13 -0300

    feat: add route layout

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,14 +2,29 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { Home } from './pages/app/home'
 import { SignIn } from './pages/auth/sign-in'
+import { AppLayout } from './pages/layouts/app'
+import { AuthLayout } from './pages/layouts/auth'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <SignIn />,
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/',
+        element: <SignIn />,
+      },
+    ],
   },
+
   {
     path: '/home',
-    element: <Home />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/home',
+        element: <Home />,
+      },
+    ],
   },
 ])
