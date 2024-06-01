c746f5319ea8e8a0f6be26b8ecfd3e7d5ce9c91c
Kássio Sá <kassio.slva@gmail.com>
2024-06-01 17:39:47 -0300

    chore: add and configure axios and react-query

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { App } from './App'
+import { App } from './app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
