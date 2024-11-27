36a6273f13c313f5d5e27598781f4b31623c13d0
Alisson Silva <alissondroid07@gmail.com>
2024-11-26 22:33:53 -0300

    feat(redesign): add new font set and logos

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,9 @@
+import LogoBrand from '../assets/logo-reduced-white.png'
+
 export function Logo({ className = 'h1 mt-3 mb-4 pb-3' }) {
   return (
     <p className={`${className} text-center text-primary`}>
-      <i className="bi bi-check-square-fill me-2"></i>
-      <u>Minhas Tarefas</u>
+      <img src={LogoBrand} alt="Company Logo" style={{ width: 90 }} />
     </p>
   )
 }
